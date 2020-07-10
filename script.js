d4581acee3f526c0efd139368b00c376b264cc1d
mndlovu18 <mndlovu@brynmawr.edu>
2020-07-10 05:40:26 -0400

    Maps!

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-// When the user clicks on div, open the popup
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+$(document).ready(function() {
+	
+	/* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }        
+    });					
 }
